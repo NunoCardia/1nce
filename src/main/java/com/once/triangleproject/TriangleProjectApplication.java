2438c2702df2a6bdbfee14cacc4fcf8355dc123e
Nuno Ferreira <nuno.ferreira@kodly.io>
2022-12-07 16:28:46 +0000

    Adds project

--- src/main/java/com/once/triangleproject/TriangleProjectApplication.java
+++ src/main/java/com/once/triangleproject/TriangleProjectApplication.java
@@ -1,4 +1,4 @@
-package com.once.demoproject;
+package com.once.triangleproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
